c59715c6621c88f8d6a9a58e55bac0827e902328
Hossep Dolatian <hovdeov@gmail.com>
2022-11-08 09:36:41 -0800

    create

---
+++ convertAlignments.py
@@ -0,0 +1,123 @@
+import pickle
+import tgt
+import glob
+import sys
+
+words_that_change_filepath = sys.argv[1]
+path = sys.argv[2]
+# Read data from file:
+print("Will read from the `wordTranscriptions` pkl file.")
+with open('wordTranscriptions.pkl', 'rb') as f:
+    wordTranscriptions = pickle.load(f)
+
+
+print("Will read the TextGrids and change the generated transcriptions from the intermediate form to the original form")
+
+print("Useful debugging info for conversion is printed into the convertAlignments.message.log")
+# The printing code was taken from https://stackoverflow.com/a/2513511
+old_stdout = sys.stdout
+log_file = open("convertAlignments.message.log", "w")
+sys.stdout = log_file
+
+# python convertAlignments.py words_that_change.json "/Users/Hovsep/Desktop/intonation_data_output_converted"
+
+if path[-1] !=  '/':
+    path = path + '/'
+
+def findClosestTranscription(transcriptionChanges,generated_intermediate_transcription):
+    closest_transcription = transcriptionChanges[0]
+    for transcriptionChange in transcriptionChanges[1:]:
+        if transcriptionChange.intermediate_transcription == generated_intermediate_transcription:
+            closest_transcription = transcriptionChange
+    return closest_transcription
+
+def replaceTranscription(transcriptionChange,generated_intermediate_transcription):
+    predicted_intermediate_transcription = transcriptionChange.intermediate_transcription
+    generated_intermediate_transcription_string = ' '.join(generated_intermediate_transcription)
+
+    original_transcription = transcriptionChange.original_transcription
+    original_transcription_string = ' '.join(original_transcription)
+    segments_that_change = transcriptionChange.segments_that_change
+
+    if generated_intermediate_transcription != predicted_intermediate_transcription:
+        print(
+            f"\tError! The TextGrid has an intermediate transcription '{generated_intermediate_transcription}' but that does not "
+            f"\tmatch the intermediate transcription that our intermediate dictionary used '{predicted_intermediate_transcription}'")
+        exit()
+
+    print(f"\t\t\tThe word '{word}' has the intermediate transcription [{generated_intermediate_transcription_string}].\n"
+          f"\t\t\tIt original transcription is [{original_transcription_string}].")
+    if generated_intermediate_transcription == original_transcription:
+        print(f"\t\t\t\tNo changes are needed because the two transcriptions are identical")
+    elif len(original_transcription) == len(generated_intermediate_transcription):
+        print(f"\t\t\tThe two transcriptions have the same length.")
+        print(f"\t\t\tThe transcriptions differ in the following changes:")
+        for segment_index in segments_that_change:
+            segment_original = original_transcription[segment_index]
+            segment_intermediate = generated_intermediate_transcription[segment_index]
+            print(
+                f"\t\t\t\tThe segment at index {segment_index} should change from [{segment_intermediate}] to [{segment_original}]")
+            if generated_intermediate_transcription[segment_index] == segment_intermediate:
+                print('\t\t\t\tOriginal:')
+                print(f"\t\t\t\t\t{phone_intervals}")
+                phone_intervals[segment_index].text = segment_original
+                print('\t\t\t\tModified:')
+                print(f"\t\t\t\t\t{phone_intervals}")
+            else:
+                print(f"\t\t\t\tError! The segment at index {segment_index} was not [{segment_intermediate}]")
+                exit()
+
+
+for textGridFilePath in glob.iglob(path + '**/*.TextGrid', recursive=True):
+    tg = tgt.read_textgrid(textGridFilePath)
+    print(f"\tProcessing the TextGrid {textGridFilePath}.")
+# Verify that the TextGrid has only two tiers: words and phones
+    if len(tg.tiers) !=  2:
+        print(f"\tError! The TextGrid must have 2 tiers, but you have {len(tg.tiers)} tier(s).")
+        exit()
+    if tg.tiers[0].name != "words":
+        print(f"\tError! The TextGrid's first tier must have the name 'words' but you have {tg.tiers[0].name}.")
+        exit()
+    wordTier = tg.get_tier_by_name("words")
+    if tg.tiers[0].name != "words":
+        print(f"\tError! The TextGrid's second tier must have the name 'phones' but you have {tg.tiers[1].name}.")
+        exit()
+    phoneTier = tg.get_tier_by_name("phones")
+
+
+    # Will go through each word and convert its phone transcription back to the original IPA symbols
+    for wordInterval in wordTier.annotations:
+        print(f"\t\tProcessing the word: {wordInterval}")
+        word = wordInterval.text
+        wordTranscription = wordTranscriptions[word]
+        transcriptionChanges=wordTranscription.transcriptions
+        phone_intervals = phoneTier.get_annotations_between_timepoints(wordInterval.start_time,
+                                                                       wordInterval.end_time)
+        generated_intermediate_transcription = [p.text for p in phone_intervals]
+        print(f"\t\tThe TextGrid has the generated intermediate transcription: the word: {' '.join(generated_intermediate_transcription)}")
+
+        if not wordTranscription.has_any_changes:
+            print(f"\t\t\tThis word cannot have changes so we don't need to process it")
+        else:
+            print(f"\t\t\tThis word can have changes so we must process it")
+            if len(transcriptionChanges)==1:
+                print(f"\t\t\tThis word can has only one possible intermediate representation in the dictionary.")
+                replaceTranscription(transcriptionChanges[0],generated_intermediate_transcription)
+            else:
+                print(f"\t\t\tThis word can has multiple possible intermediate representation in the dictionary.\n"
+                      f"\t\t\tThey are")
+                for transcriptionChange in transcriptionChanges:
+                    print(transcriptionChange)
+                print(f"\t\t\tWe must find the only identical one.")
+                closest_transcription = findClosestTranscription(transcriptionChanges, generated_intermediate_transcription)
+                print(f"\t\t\tThe closest transcription is:")
+                print(closest_transcription)
+                replaceTranscription(closest_transcription,generated_intermediate_transcription)
+    print(f"\tThe new TextGrid is saved and replaced the original")
+    tgt.write_to_file(tg,textGridFilePath,format='long')
+
+
+sys.stdout = old_stdout
+log_file.close()
+
+print("The conversion is complete.")
---
+++ convertPronDict.py
@@ -0,0 +1,120 @@
+import codecs
+import pickle
+import sys
+
+from WordTranscriptionAndChanges import *
+
+pronDict_original_filepath = sys.argv[1]
+phone_mapping_filepath = sys.argv[2]
+pronDict_intermediate_filepath = sys.argv[3]
+
+#  python convertPronDict.py  /Users/Hovsep/Desktop/word-pronunciations-WA.txt /Users/Hovsep/Desktop/phoneMapping.txt /Users/Hovsep/Desktop/pronDictIntermediate.txt
+
+
+# Read the phone_mapping file and create a dict object out of it
+phone_mapping = {}
+# The phone_mapping dictionary has a simple structure of: phone_mapping[original_segment]=intermediate_segment
+with codecs.open(phone_mapping_filepath, 'r', 'utf-8') as phone_mapping_file:
+    print("Will read the phone mapping file.")
+    phone_lines = phone_mapping_file.read().splitlines()
+    phone_lines[0] = phone_lines[0].replace('\ufeff', "")
+    for phone_line in phone_lines:
+        phone_line = phone_line.split('\t')
+
+        if len(phone_line) != 2:
+            print(f"Error, the line `{phone_line}` in the phone mapping file is NOT tab-separated with two columns.")
+            exit()
+
+        original_phone, intermediate_phone = phone_line
+        if original_phone in phone_mapping:
+            print(f"Error, the phone {original_phone} appears multiple times in the phone mapping file.")
+            exit()
+        phone_mapping[original_phone] = intermediate_phone
+
+
+# Read the original pronunciation dictionary that had the original transcriptions.
+# Create a new dictionary with intermediate transcriptions by applying the custom phone mapping
+# Store information on the transcriptions that changed into a dict called `wordTranscriptions`
+wordTranscriptions = {}
+
+# The transcription_conversions dict  is made up of words whose transcriptions did or did not change.
+# Each word maps to a wordTranscription object.
+# A wordTranscription object contains the word's original transcription, intermediate transcriptions, and the list of segment changes
+# e.g. = transcription_conversions =  { "ըսաւ": obj0,   "ասաց": obj1,  "բառ": obj2,  "րա": obj3 ,  "ռա": obj3 }
+# }
+# The word ըսաւ has an unchanged transcription form ["ə","s","ɑ","v"]
+# The word ասաց has a single alteration from ["ɑ","s","ɑ","t͡sʰ"] to ["ɑ","s","ɑ","tʃ"] at index 3
+# The word բառ has a single intermediate forms with two alternations, from  ["p", "ʏ", "r"] to ["p", "u", "ɾ"] at indexes 1,2
+# The word րա has two intermediate forms, with zero or one alteration,
+#   from ["ɾ","ɑ"] to ["ɾ","ɑ"] with zero changes, or  ["ɻ","ɑ"] to ["j","ɑ"] with change at index 0
+# The word ռա has two identical intermediate forms from ["ɾ","ɑ"] or ["r","ɑ"] to ["ɾ","ɑ"] at index 0
+
+
+def convert_transcription_to_intermediate(original_transcription):
+    # Convert an original transcription into the intermediate transcription (even if identical) by using the custom phone_mapping dict.
+    # Keep track of the changes if any
+
+    # transcriptionChange:  original_transcription, intermediate_transcription, segments_that_change, didChange):
+    intermediate_transcription = []
+    segments_that_change = []
+    for i in range(len(original_transcription)):
+        original_segment = original_transcription[i]
+        if original_segment in phone_mapping:
+            intermediate_segment=phone_mapping[original_segment]
+            intermediate_transcription.append(intermediate_segment)
+            segments_that_change.append(i)
+        else:
+            intermediate_transcription.append(original_segment)
+    transcriptionChange = TranscriptionChange(original_transcription,intermediate_transcription,segments_that_change)
+    return transcriptionChange
+
+print("Will read the original pronunciation dictionary and create an intermediate pronunciation dictionary")
+# Read the original pronunciation dictionary and create a modified dictionary with English IPA symbols
+
+print("Useful debugging info for conversion is printed into the convertPronDict.message.log")
+# The printing code was taken from https://stackoverflow.com/a/2513511
+# old_stdout = sys.stdout
+# log_file = open("convertPronDict.message.log", "w")
+# sys.stdout = log_file
+
+
+with codecs.open(pronDict_original_filepath, 'r', 'utf-8') as pronDict_original_file:
+    with codecs.open(pronDict_intermediate_filepath, 'w', 'utf-8') as pronDict_intermediate_file:
+        dictionary_lines = pronDict_original_file.read().splitlines()
+        dictionary_lines[0] = dictionary_lines[0].replace('\ufeff', "")
+        for dictionary_line in dictionary_lines:
+            print(f"\tProcessing the following line from the original pronunciation dictionary: {dictionary_line}")
+            dictionary_line = dictionary_line.split('\t')
+            if len(phone_line) != 2:
+                print(f"\tError, the line `{dictionary_line}` in the original pronunciation dictionary file is NOT tab-separated with two columns.")
+                exit()
+
+            word,original_transcription_string = dictionary_line
+            original_transcription = original_transcription_string.split(' ')
+            transcriptionChange=convert_transcription_to_intermediate(original_transcription)
+            intermediate_transcription_string = ' '.join(transcriptionChange.intermediate_transcription)
+            if word not in wordTranscriptions:
+                print(f"\t\tThe word {word} is newly encountered")
+                newWordTranscription = WordTranscription(word)
+                print(f"\t\tIts original transcription {original_transcription_string} is converted to {intermediate_transcription_string}.")
+                newWordTranscription.add_transcription(transcriptionChange)
+                wordTranscriptions[word] = newWordTranscription
+            else:
+                print(f"\t\tThe word {word} is previously encountered")
+                print(f"\t\tIts original transcription {original_transcription_string} is converted to {intermediate_transcription_string}.")
+                wordTranscriptions[word].add_transcription(transcriptionChange)
+
+            pronDict_intermediate_file.write(f"{word}\t{intermediate_transcription_string}\n")
+
+print(f"The word transcriptions are as follows:")
+for wordTranscription in wordTranscriptions.items():
+    print(str(wordTranscription[1]))
+
+# sys.stdout = old_stdout
+# log_file.close()
+
+print("The pickle object `wordTranscriptions.pkl` is created. It is a Python dictionary that keeps track of the changes.")
+with open('wordTranscriptions.pkl', 'wb') as f:
+    pickle.dump(wordTranscriptions, f)
+
+
